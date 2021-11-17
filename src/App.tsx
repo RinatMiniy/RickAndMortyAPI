7e0b8bce92e0991018fea4b2e4552c5c6417ddcf
RinatMiniy <RinatMiniy@gmail.com>
2021-11-17 13:05:37 +0300

    init: add webpack

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { CardsField } from "./components/CardsField/CardsField"
+import { SerchBar } from "./components/SearchBar/SearchBar"
+
+export const App = () => {
+  return (
+    <>
+      <SerchBar />
+      <CardsField />
+    </>
+  )
+}
